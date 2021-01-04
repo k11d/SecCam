3165ea9a1548828d08676585d258b4a3cbb844c9
Ilic Danijel - k11d <danijel.ilic91@gmail.com>
2021-01-04 18:46:39 +0100

    proof of concept

--- client.py
+++ client.py
@@ -4,21 +4,63 @@ import sys
 import socket
 from PIL import Image
 import numpy as np
+from io import BytesIO
+import random
+import time
+import pickle
 
+
+
+def _try_import_opencv():
+    try:
+        import cv2
+    except ImportError:
+        return False
+    else:
+        cv2.namedWindow(str(device_id), cv2.WINDOW_GUI_NORMAL)
+        return True
+
+
+def show(frame):
+    cv2.imshow(str(device_id), frame)
+    if cv2.waitKey(2) == 27:
+        cv2.destroyAllWindows()
+        sys.exit(0)
+
+
+device_id = sys.argv[1]
+_show = _try_import_opencv()
+if _show:
+    import cv2
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('172.22.22.65', 9999)
+server_address = ('0.0.0.0', 6666)
 print('connecting to {} port {}'.format(*server_address))
 sock.connect(server_address)
+connected = True
+while connected:
+    data = b""
+    # device_id = 0 if random.random() < 0.5 else 1
+    sock.send(str(device_id).encode('utf-8') + b" " * 127)
+    while not data.endswith(b"\n\n\n\n"):
+        try:
+            chunk = sock.recv(64)
+            if chunk:
+                data += chunk
+            else:
+                print("no data")
+                break
+        except:
+            print("Lost server connection")
+            connected = False
+            break        
+    try:
+        # data = data[:-4] # not needed
+        img = pickle.loads(data)
+        if _show: show(img)
+    except Exception as e:
+        print(e)
+    time.sleep(1)
 
-# send command
-data = np.array(Image.open(sys.argv[1])).tobytes()
-sock.sendall(data)
-
-# amount_received = 0
-# amount_expected = len(message)
-
-ak = int(sock.recv(64))
-print('received:', ak)
 sock.close()
--- server.py
+++ server.py
@@ -1,58 +1,49 @@
-# server.py
-
-import socket
-import sys
-from io import BytesIO
+from socket import *
+import time
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+import numpy as np
 from PIL import Image
+import pickle
 
 
-def start_server(address=('0.0.0.0', 6666)):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print('starting up on {} port {}'.format(*address))
+_pool = ThreadPoolExecutor(10)
+_lock = Lock()
+
+
+def start_server(address):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     sock.bind(address)
-    return sock
+    sock.listen(5)
 
-
-def listen(sock):
-    sock.listen(1)
     while True:
-        print('waiting for a connection')
-        connection, client_address = sock.accept()
-        handle_client(connection, client_address, server_task)
+        client, addr = sock.accept()
+        print("Serving:", addr)
+        _pool.submit(handler, client)
 
 
-def server_task():
-    return b"HelloWorld"
-
-
-def handle_client(connection, address, task, keep_alive=False):
-    data = bin()
+def handler(client):
     while True:
+        req = client.recv(128)
+        sreq = req.decode('utf-8')
+        print("Client request:", sreq)
         try:
-            print('connection from', address)
-            while True:
-                chunk = connection.recv(16)
-                if chunk:
-                    print('received {!r}'.format(chunk))
-                    #connection.sendall(data)
-                    data += chunk
-                else:
-                    print('no data from', address)
-                    break
-        finally:
-            if not keep_alive:
-                connection.close()
-            # if data:
-            #     data = BytesIO(_data).read()
-                outname = 'data_' + address + '.png'
-                with open(outname, 'wb') as f:
-                    f.write(data)
+            with _lock:
+                data = fetch_data(int(sreq))
+            client.send(data)
+        except:
+            client.close()
+            break
 
 
+def fetch_data(id):
+    if id == 0:
+        idata = pickle.dumps(np.array(Image.open('frame_0.png')))
+    elif id == 1:
+        idata = pickle.dumps(np.array(Image.open('frame_1.png')))
+    data = idata + b"\n\n\n\n"
+    return data
 
-def run():
-    s = start_server()
-    listen(s)
 
-run()
-
+X = 0
+start_server(('0.0.0.0', 6666))
